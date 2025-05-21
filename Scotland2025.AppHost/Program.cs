d4f0cd7400fe1ce6a7265bc14a83fadf71a9611a
Daniel Nichols <danielrnichols63@gmail.com>
2025-05-21 07:20:14 -0500

    Adds UI to AppHost

--- Scotland2025.AppHost/Program.cs
+++ Scotland2025.AppHost/Program.cs
@@ -1,5 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Scotland2025_Api>("scotland2025-api");
+var api = builder.AddProject<Projects.Scotland2025_Api>("scotland2025-api");
+
+builder.AddProject<Projects.Scotland2025_UI>("scotland2025-ui")
+    .WaitFor(api);
+
 
 builder.Build().Run();

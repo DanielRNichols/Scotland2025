59e9661cfb5ed8b6fa01004604c95f135ec58940
Daniel Nichols <danielrnichols63@gmail.com>
2025-06-07 10:30:43 -0500

    Formatting

--- Scotland2025.AppHost/Program.cs
+++ Scotland2025.AppHost/Program.cs
@@ -2,4 +2,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Scotland2025>("scotland2025");
 
+builder.AddProject<Projects.Scotland2025_Api>("scotland2025-api");
+
+builder.AddProject<Projects.Scotland2025_JsonDocumentPoster>("scotland2025-jsondocumentposter");
+
 builder.Build().Run();
